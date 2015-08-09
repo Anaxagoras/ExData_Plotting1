7d48fffab2392379d6c2e6446fb64d20d5e60d22
David Edelstein <euclid11@gmail.com>
2015-08-09 16:27:30 -0700

    One graph fixed

--- plot1.R
+++ plot1.R
@@ -1,21 +1,23 @@
-consUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-download.file(consUrl, "household_power_consumption.zip", method = "curl")
-
-consumption <- read.table(
-    unz("household_power_consumption.zip", "household_power_consumption.txt"),
-    header = TRUE,
-    sep = ";")
-
-consumption$Time <- paste(consumption$Date, consumption$Time)
-consumption$Date <- as.Date(levels(consumption$Date), format = "%d/%m/%Y")[consumption$Date]
-consLimit <- subset(consumption,
-                    (Date >= as.Date("2007-02-01", "%Y-%m-%d")) 
-                    & (Date <= as.Date("2007-02-02", "%Y-%m-%d")))
-consLimit$Global_active_power <- as.numeric(levels(consLimit$Global_active_power))[consLimit$Global_active_power]
-
-png(filename="plot1.png", width=480, height=480)
-hist(consLimit$Global_active_power,
-     col = "red",
-     main = "Global Active Power",
-     xlab = "Global Active Power (kilowatts)")
-dev.off()
+makePlot1 <- function() {
+    consUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(consUrl, "household_power_consumption.zip", method = "curl")
+    
+    consumption <- read.table(
+        unz("household_power_consumption.zip", "household_power_consumption.txt"),
+        header = TRUE,
+        sep = ";")
+    
+    consumption$Time <- paste(consumption$Date, consumption$Time)
+    consumption$Date <- as.Date(levels(consumption$Date), format = "%d/%m/%Y")[consumption$Date]
+    consLimit <- subset(consumption,
+                        (Date >= as.Date("2007-02-01", "%Y-%m-%d")) 
+                        & (Date <= as.Date("2007-02-02", "%Y-%m-%d")))
+    consLimit$Global_active_power <- as.numeric(levels(consLimit$Global_active_power))[consLimit$Global_active_power]
+    
+    png(filename="plot1.png", width=480, height=480)
+    hist(consLimit$Global_active_power,
+         col = "red",
+         main = "Global Active Power",
+         xlab = "Global Active Power (kilowatts)")
+    dev.off()
+}
